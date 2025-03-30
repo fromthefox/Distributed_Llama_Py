084922a32cb7958574f732d94a315a387bcbb782
Yanhui Bian <fromthefox@icloud.com>
2025-03-30 14:49:36 +0800

    Update the framework

--- model_inference_main.py
+++ model_inference_main.py
@@ -4,3 +4,9 @@ here we accept the param from Auto-Deployment Proj.
 :param accepted: list, like [25, 48, 55] for unsplitted-dim == 128
 """
 
+def infenerce_framework(allocation_list:list):
+    """
+    the whole framework of the project.
+    :parma allocation_list: list, like [25, 48, 55] for unsplitted-dim == 128
+    :return: None, just do the inference
+    """
