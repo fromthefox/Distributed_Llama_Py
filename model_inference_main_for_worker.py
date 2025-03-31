aebfa87a8443e90cacbb2d205b0418ca11479ca9
Yanhui Bian <fromthefox@icloud.com>
2025-03-31 14:55:56 +0800

    Update the inference_main_for_worker

--- model_inference_main_for_worker.py
+++ model_inference_main_for_worker.py
@@ -4,21 +4,10 @@ this file is used to finish the inference task for worker node
 import socket
 from socket_client import TCPClient
 
-"""
-if __name__ == "__main__":
-    clients = []
-    # for _ in range(3):  # 模拟多个客户端
-    #     client = TCPClient()
-    #     threading.Thread(target=client.start_handling).start()
-    #     clients.append(client)
-    client = TCPClient()
-    client.start_handling()
-"""
-
-
 def inference_main_for_worker():
     """
     this function is used to finish the inference task for worker node
     """
-    client = TCPClient()
+    host, port = "192.168.1.1", 44444
+    client = TCPClient(host, port)
     client.start_handling()
