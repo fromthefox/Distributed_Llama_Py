b0659d750b4e20db8d630346797821b706972551
Yanhui Bian <fromthefox@icloud.com>
2025-03-31 16:23:39 +0800

    fix the bug

--- model_inference_main_for_server.py
+++ model_inference_main_for_server.py
@@ -9,7 +9,7 @@ import threading
 import model_inference_framework
 
 
-def infenerce_main_for_server(allocation_list:list, model_path:str, tokenizer_path:str, config_path:str, user_config_path:str) -> None:
+def infenerce_main_for_server(allocation_list:list, model_path:str, tokenizer_path:str, config_path:str, user_config_path:str) -> str:
     """
     the whole framework of the project.
     :parma allocation_list: list, like [25, 48, 55] for unsplitted-dim == 128
