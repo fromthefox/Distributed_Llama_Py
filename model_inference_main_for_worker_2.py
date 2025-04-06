06afda45248b1566a73ea9c4d4b7e837cc4fa59e
fox <fromthefox@icloud.com>
2025-04-06 16:20:34 +0800

    Update test code

---
+++ model_inference_main_for_worker_2.py
@@ -0,0 +1,15 @@
+"""
+this file is used to finish the inference task for worker node
+"""
+import socket
+from socket_client_2 import TCPClient
+
+def inference_main_for_worker():
+    """
+    this function is used to finish the inference task for worker node
+    """
+    host, port = "192.168.1.104", 44444
+    client = TCPClient(host, port)
+    client.start_handling()
+
+inference_main_for_worker()
--- socket_client_2.py
+++ socket_client_2.py
@@ -1,19 +1,29 @@
 import socket
 import threading
-from socket_comm_module import send_message, receive_message, pack_tensor,unpack_tensor
+from socket_comm_module import send_message, receive_message
+from socket_comm_module import unpack_tensor, pack_tensor
 import torch
 
-
 class TCPClient:
-    """客户端实现"""
+    """
+    Client-side implementation
+    """
     def __init__(self, host: str = '192.168.1.104', port: int = 44444):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(("192.168.1.104", 33333))
+        
+        # Bind to a specific address and port (optional), make sure to use a different port than the server
+        self.sock.bind(("192.168.1.105", 45678))
+
+        # Connect to the server
         self.sock.connect((host, port))
+
+        # Set the socket to non-blocking mode (optional)
         self.running = True
 
     def start_handling(self):
-        """启动请求处理循环"""
+        """
+        Starting the request processing loop
+        """
         while self.running:
             try:
                 data = receive_message(self.sock)
@@ -22,6 +32,12 @@ class TCPClient:
                 
                 data_tensor = unpack_tensor(data)
                 if len(data_tensor.shape) == 2:
+                    """
+                    here we can also pack some str with the data to differ the tensor type from input_embedding and matrix
+                    PLAN A: add a flag to the tensor to indicate its type
+                    PLAN B: use a dictionary to pack the tensor and the type
+                    """
+                    # if the tensor is 2D, it is the input embedding
                     input_embedding = data_tensor
                     print('Received')
                     response = b'Received'
@@ -29,6 +45,7 @@ class TCPClient:
                     matrix = data_tensor
                     matrix_res = torch.empty(matrix.shape[0], input_embedding.shape[0], matrix.shape[1])
                     if input_embedding == None:
+                        # we must ensure that the input_embedding is not None
                         raise ValueError("input_embedding is None!!")
                     else:
                         for i in range(matrix.shape[0]):
@@ -40,12 +57,3 @@ class TCPClient:
                 break
 
         self.sock.close()
-
-if __name__ == "__main__":
-    clients = []
-    # for _ in range(3):  # 模拟多个客户端
-    #     client = TCPClient()
-    #     threading.Thread(target=client.start_handling).start()
-    #     clients.append(client)
-    client = TCPClient()
-    client.start_handling()
