7410ff4d0a8fcbecefa7d303b9828a8936198265
fox <fromthefox@icloud.com>
2025-04-08 10:23:29 +0800

    Update the pack_tensor and unpack_tensor.

--- socket_comm_module.py
+++ socket_comm_module.py
@@ -51,11 +51,46 @@ def read_network_config(network_config: dict):
     ports_list = network_config["ports_list"]
     return addrs_list, ports_list
 
-def pack_tensor(tensor):
-    buffer = io.BytesIO()
-    torch.save(tensor, buffer)
-    tensor_bytes = buffer.getvalue()
-    return tensor_bytes
+def pack_tensor(tensor, computation_time = None, message_type = "DATA"):
+    """
+    Pack a tensor and time info into bytes.
+    Args:
+        tensor: The tensor to be packed.
+        computation_time: The time taken for computation (optional).
+        message_type: The type of message (optional).
+    Returns:
+        tensor_bytes: The packed data as bytes.
+    """
+    # init the dict
+    data_package = {
+        "tensor": tensor,
+        "message_type": message_type
+    }
 
-def unpack_tensor(tensor_bytes):
-    return torch.load(io.BytesIO(tensor_bytes))
+    if computation_time is not None:
+        data_package["computation_time"] = computation_time
+        data_package["message_type"] = "TIMING"
+    data_package_bytes = pickle.dumps(data_package)
+    return data_package_bytes
+
+def unpack_tensor(data_package_bytes):
+    """
+    Args:
+        packed_data: bytes data
+
+    Returns:
+        tensor: The unpacked tensor.
+        computation_time: The computation time (if present).
+        message_type: The type of message (if present).
+    """
+    # Unpack the data
+    data_package = pickle.loads(data_package_bytes)
+
+    # get tensor
+    tensor_data = data_package.get("tensor")
+
+    message_type = data_package.get("message_type", "DATA")
+
+    computation_time = data_package.get("computation_time", None)
+
+    return tensor_data, message_type, computation_time
