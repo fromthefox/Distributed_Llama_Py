76ae9e80062cc745c30a19242a1e53c316f304bb
Yanhui Bian <fromthefox@icloud.com>
2025-04-13 14:50:46 +0800

    End of experiment for now.

--- model_inference_module.py
+++ model_inference_module.py
@@ -342,10 +342,10 @@ def cal_new_dynamic_ratio(computation_time_list:list, translation_time_list:list
     translation_ratio = (translation_sum_time / total_time)
 
     ratio = max(computation_ratio, translation_ratio)
-    ratio_final = min(max(ratio, 0.05), 0.95)
+    ratio_final = min(max(ratio, 0.1), 0.9)
     
     
-    return ratio_final
+    return 1-ratio_final
 
 def dynamic_weights_dis(dynamic_weights:np.ndarray, base_weights:np.ndarray, dynamic_ratio = 0.05) -> np.ndarray:
     """
@@ -420,15 +420,17 @@ def cal_new_base_weights(computation_time_list:list, translation_time_list:list)
 
     return new_base_weights
 
-nodes_info_dict = {'arithmetic': [631, 631, 2301, 2301], 'memory': [8, 8, 350, 350], 'bandwidth': [76, 76, 56, 56]}
-computation_time_list = [0.9025006659998382, 0.947418752000118, 0.6442230499815196, 0.9484541683923453]
-translation_time_list = [164.04131133396876, 203.54729014797599, 1117.1440215499897, 1135.9753773316334]
-dynamic_part = [0.33333333, 0.33333333, 0.33333333]
-new_base_weights = cal_new_base_weights(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
-dynamic_weights_array = dynamic_weights_dis(dynamic_weights=dynamic_part, base_weights=new_base_weights)
-scores_list = total_score_dis(nodes_info_dict, dynamic_weights_array)
-allocation_list = proportinal_allocation_dis(scores_list, 128)
-print(f"Dynamic Part: {dynamic_part}")
-print(f"Dynamic Weights: {dynamic_weights_array}")
-print(f"Final Scores: {scores_list}")
-print(f"Allocation List: {allocation_list}")
+# nodes_info_dict = {'arithmetic': [631, 631, 2301, 2301], 'memory': [8, 8, 350, 350], 'bandwidth': [160, 160, 80, 80]}
+# computation_time_list = [0.8460162929996926, 0.8853536270001321, 0.5917502362281084, 0.5708553295116872]
+# translation_time_list = [175.27169220706753, 236.51146707308698, 365.3462711638422, 357.4909621703555]
+# dynamic_part = [0.33333333, 0.33333333, 0.33333333]
+# new_base_weights = cal_new_base_weights(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
+# new_dynamic_ratio = cal_new_dynamic_ratio(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
+# dynamic_weights_array = dynamic_weights_dis(dynamic_weights=dynamic_part, base_weights=new_base_weights, dynamic_ratio=new_dynamic_ratio)
+# scores_list = total_score_dis(nodes_info_dict, dynamic_weights_array)
+# allocation_list = proportinal_allocation_dis(scores_list, 128)
+# print(f"new_dynamic_ratio: {new_dynamic_ratio}")
+# print(f"Dynamic Part: {dynamic_part}")
+# print(f"Dynamic Weights: {dynamic_weights_array}")
+# print(f"Final Scores: {scores_list}")
+# print(f"Allocation List: {allocation_list}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,14 @@
-
-    #     raise ValueError("scores_list cannot be empty")
+31, 2301, 2301], 'memory': [8, 8, 350, 350], 'bandwidth': [160, 160, 80, 80]}
+computation_time_list = [0.8460162929996926, 0.8853536270001321, 0.5917502362281084, 0.5708553295116872]
+translation_time_list = [175.27169220706753, 236.51146707308698, 365.3462711638422, 357.4909621703555]
+dynamic_part = [0.33333333, 0.33333333, 0.33333333]
+new_base_weights = cal_new_base_weights(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
+new_dynamic_ratio = cal_new_dynamic_ratio(computation_time_list=computation_time_list, translation_time_list=translation_time_list)
+dynamic_weights_array = dynamic_weights_dis(dynamic_weights=dynamic_part, base_weights=new_base_weights, dynamic_ratio=new_dynamic_ratio)
+scores_list = total_score_dis(nodes_info_dict, dynamic_weights_array)
+allocation_list = proportinal_allocation_dis(scores_list, 128)
+print(f"new_dynamic_ratio: {new_dynamic_ratio}")
+print(f"Dynamic Part: {dynamic_part}")
+print(f"Dynamic Weights: {dynamic_weights_array}")
+print(f"Final Scores: {scores_list}")
+print(f"Allocation List: {allocation_list}")
